3ab1d1065a5e3521016d93d586c7b14f23a0de14
wachehi69 <wachehi6905@gmail.com>
2021-09-02 15:07:01 +0200

    ajout des tests

--- src/test/java/org/mairie/comores/GestionMairieComoresApplicationTests.java
+++ src/test/java/org/mairie/comores/GestionMairieComoresApplicationTests.java
@@ -1,19 +1,25 @@
 package org.mairie.comores;
 
+import org.junit.Test;
+
 //import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mairie.comores.web.MacClasse;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import junit.framework.TestCase;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class GestionMairieComoresApplicationTests {
+public class GestionMairieComoresApplicationTests extends TestCase{
 
-	// test1 //test2   //test3
-@Test
-	public void contextLoads() {
-	}
+
+
+	@Test    
+	public void testCalculer() throws Exception {
+	    assertEquals(2,MacClasse.calculer(1,1));
+	  }
 
 }
